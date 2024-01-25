3910bb9ec01bd000a3fd4b45b107054fc6544584
codigobenja <35717432+codigobenja@users.noreply.github.com>
2024-01-25 01:37:05 -0700

    Update encriptacion.js

--- assets/js/Encriptacion/encriptacion.js
+++ assets/js/Encriptacion/encriptacion.js
@@ -36,3 +36,20 @@ function copiar(){
     navigator.clipboard.writeText(codigoACopiar);
 }
 var boton3 = document.querySelector("#btn-copy"); boton3.onclick = copiar;
+
+function SoloLetras(e){
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toString();
+    letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    especiales = [8,13];
+    tecla_especial = false
+    for(var i in especiales){
+        if(key == especiales[i]){
+            tecla_especial = true;
+            break;
+        }
+    }
+    if(letras.indexOf(tecla) == -1 && !tecla_especial){
+        return false;
+    }
+}
